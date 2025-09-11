c4daf012220edd83e84868763e8626e74f410156
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-09-11 16:39:32 +0000

    Final Commit

---
+++ final_attendees.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+df = pd.read_csv('ticket_data.csv',encoding='latin-1') 
+df2 = df.drop_duplicates() 
+columnsToRemove = ['Order ID','Order date','COUNTIF','Phone number','Purchaser city','Purchaser state',
+                  'Purchaser country','Event name','Event ID','Event start date',	'Event start time', 'Event timezone','Event location','Ticket tier','Buyer first name','Buyer last name','Buyer email','Ticket type','Currency','Attendee email']
+newDF = df2.drop(columnsToRemove,axis=1) 
+print(newDF.to_string())
