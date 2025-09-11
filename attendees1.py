302f0896177b1081484af0574625ab6af5aef185
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-09-11 17:26:18 +0100

    Update attendees1.py

--- attendees1.py
+++ attendees1.py
@@ -2,8 +2,7 @@ import pandas as pd
 df = pd.read_csv('ticket_data.csv',encoding='latin-1') 
 df1 = df.drop_duplicates() 
 columnsToRemove = ['Order ID','Order date','COUNTIF','Phone number','Purchaser city','Purchaser state',
-                  'Purchaser country','Event name','Event ID','Event start date',	'Event start time', 'Event timezone',	'Event location',
-'Ticker tier','Buyer first name','Buyer last name','Buyer email','Ticket type','Currency']
+                  'Purchaser country','Event name','Event ID','Event start date',	'Event start time', 'Event timezone','Event location','Ticker tier','Buyer first name','Buyer last name','Buyer email','Ticket type','Currency']
 newDF = df1.drop(columnsToRemove,axis=1) 
 print(newDF.to_string())
                    
