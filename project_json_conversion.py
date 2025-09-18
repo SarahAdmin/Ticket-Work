a8fdfb17ca72466f2d5c4b0fd900c699cb245f30
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-09-18 20:06:23 +0000

    Final Committ 2

--- project_json_conversion.py
+++ project_json_conversion.py
@@ -1,3 +1,7 @@
 import pandas as pd 
 df = pd.read_csv('ticket_data.csv',encoding='latin-1') 
-newDF = df.to_json('ticket_data_1.json')
+df = df.drop_duplicates()
+columnsToRemove = ['Order ID','Order date','COUNTIF','Phone number','Purchaser city','Purchaser state',
+                  'Purchaser country','Event name','Event ID','Event start date',	'Event start time', 'Event timezone','Event location','Ticket tier','Buyer first name','Buyer last name','Buyer email','Ticket type','Currency','Attendee email']
+df = df.drop(columnsToRemove,axis=1) 
+finalDF = df.to_json('ticket_data_1.json')
