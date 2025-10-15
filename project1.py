e7f39ba13c22fc24aa340620b7c3db059e9ac4ca
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-10-15 22:56:01 +0100

    Implement read_ticket_data function
    Add function to read ticket data from a CSV file with error handling.

---
+++ project1.py
@@ -0,0 +1,15 @@
+import pandas as pd
+def read_ticket_data(file_path):
+  try: 
+    df = pd.read_csv('ticket_data.csv',encoding='latin-1) 
+    print(f'Successfully read {lean(df)} rows from the file.') 
+    return df 
+  except FileNotFoundError: 
+       print(f'Error: File is not found at path: {file_path}') 
+       return None 
+  except Exception as e:
+    print(f'Error occurred while the reading the CSV {e}') 
+    return None 
+    
+      
+  
