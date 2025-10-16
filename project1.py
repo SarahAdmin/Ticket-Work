3c1a72a9c0455542afdf43bf163904ca1cb40810
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-10-16 21:37:38 +0100

    Fix typo in row count print statement

--- project1.py
+++ project1.py
@@ -2,7 +2,7 @@ import pandas as pd
 def read_ticket_data(file_path):
   try: 
     df = pd.read_csv('ticket_data.csv',encoding='latin-1) 
-    print(f'Successfully read {lean(df)} rows from the file.') 
+    print(f'Successfully read {len(df)} rows from the file.') 
     return df 
   except FileNotFoundError: 
        print(f'Error: File is not found at path: {file_path}') 
